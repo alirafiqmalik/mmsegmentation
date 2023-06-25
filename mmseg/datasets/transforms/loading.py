f6c048f41127f39facddf8da617a57b8c89aa2e8
Ali Hamza Malik <88540402+alirafiqmalik@users.noreply.github.com>
2023-06-26 00:53:26 +0500

    Removed debugging print and updated annotation path (hardcoded solution for Floodnet dataset)

--- mmseg/datasets/transforms/loading.py
+++ mmseg/datasets/transforms/loading.py
@@ -91,10 +91,8 @@ class LoadAnnotations(MMCV_LoadAnnotations):
             dict: The dict contains loaded semantic segmentation annotations.
         """
 
-        print("HERE ",results['seg_map_path'])
-
         img_bytes = fileio.get(
-            results['seg_map_path'], backend_args=self.backend_args)
+            results['seg_map_path'].split(".")[0]+"_lab.png", backend_args=self.backend_args)
         gt_semantic_seg = mmcv.imfrombytes(
             img_bytes, flag='unchanged',
             backend=self.imdecode_backend).squeeze().astype(np.uint8)
